a654455df54d699564e45cbbff054e4f0069f8de
aptennn <uegor@protonmail.com>
2023-10-08 14:02:26 +0300

    Change hello

--- hello.pas
+++ hello.pas
@@ -1,3 +1,12 @@
 ﻿begin
-  print('Hello, world');
+  Println('Whats your language?'); 
+  var a := ReadInteger('1-English, 2-Русский, 3-Deutsch, 4-Español');
+ 
+  case a of
+    1: Print('Hello World');
+    2: Print('Привет, мир');
+    3: Print('Hallo Welt');
+    4: Print('Hola Mundo');
+    else Print('I do not know your language (6');
+  end;
 end.
